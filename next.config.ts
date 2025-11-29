df01223a6fd1a0a47b19f28f3faeda855ad6dcc8
habyarimana caleb <callebhabyar55@gmail.com>
2025-11-30 00:33:09 +0200

    Make PWA read for production and development

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,15 @@ const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
+  // disable: process.env.NODE_ENV === "development",
+   disable: false, // allowPWA in both dev and prod
+
 });
 
 module.exports = withPWA({
+  experimental: {
+    appDir: true, // enable App Router
+  },
   async rewrites() {
     return [
       {
