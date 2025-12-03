4f7a8bf484b93c65b0ae9e80f3116e7d3e130739
juniorally910 <juniorally910@gmail.com>
2025-12-03 20:36:21 +0200

    pulling from school branch

--- components/onboard/Dashboard.tsx
+++ components/onboard/Dashboard.tsx
@@ -71,7 +71,7 @@ export default function Dashboard({ onLogout }: DashboardProps) {
 
   const handleUpdateSchool = async (updatedSchool: Omit<School, 'id' | 'status' | 'registrationDate'>) => {
     if (editingSchool) {
-      await fetch(`http://localhost:5000/api/schools/${editingSchool.id}`, {
+      await fetch(`/api/users/schools/${editingSchool.id}`, {
         method: "PUT",
         headers: {"content-type": "application/json"},
         body: JSON.stringify(updatedSchool)
@@ -84,7 +84,7 @@ export default function Dashboard({ onLogout }: DashboardProps) {
   };
 
   const handleRequestApproval = async (schoolId: string) => {
-   await fetch(`http://localhost:5000/api/schools/${schoolId}/status`, {
+   await fetch(`/api/users/schools/${schoolId}/status`, {
       method: "PATCH",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ status: "Pending Approval" }),
